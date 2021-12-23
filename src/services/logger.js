139010ee9ddc66c5fdcaf54fd9495c4ba3cf962a
Ruslan Bielyi <ruslan_bielyi@epam.com>
2021-12-23 14:10:28 +0200

    [EDP-DDM-815] extended HTTP logging for TRACE log level
    Change-Id: I5b855f28fde428263f21f123fd851041c1c00739

--- src/services/logger.js
+++ src/services/logger.js
@@ -1,7 +1,6 @@
 /*
  * Derivative Work based on <Form.IO> library and released under OSL-3.0 license.
  */
-
 const { format, transports, createLogger } = require('winston');
 const expressWinston = require('express-winston');
 
@@ -18,13 +17,19 @@ class LoggerService {
       expressWinston.responseWhitelist.push('body');
     }
 
-    this.middleware = expressWinston.logger({
-      ...this.loggerOptions,
-      meta: true, // optional: control whether you want to log the meta data about the request (default to true)
-      headerBlacklist: this.headersBlackList,
-      ignoreRoute: (req) => req.path.includes('/actuator'),
-      msg: 'HTTP {{req.method}} {{res.statusCode}} {{req.url}} - {{res.responseTime}}ms',
-    });
+    this.middleware = (req, res, next) => {
+      this.routesBlackList = ['/actuator'];
+      if (this.logLevel === 'TRACE') {
+        this.logHttpTrace(req, res);
+      }
+      return expressWinston.logger({
+        ...this.loggerOptions,
+        meta: true, // optional: control whether you want to log the meta data about the request (default to true)
+        headerBlacklist: this.headersBlackList,
+        ignoreRoute: (req) => this.routesBlackList.some((url) => req.path.includes(url)),
+        msg: 'HTTP {{req.method}} {{res.statusCode}} {{req.url}} - {{res.responseTime}}ms',
+      })(req, res, next);
+    };
     this.logger = createLogger(this.loggerOptions);
 
     process.on('uncaughtException', (err) => {
@@ -36,10 +41,69 @@ class LoggerService {
     });
   }
 
+  logHttpTrace(request, response) {
+    if (this.routesBlackList.some((url) => request.originalUrl.includes(url))) {
+      return;
+    }
+    let body = [];
+    let requestErrorMessage = null;
+    const logRequestChunk = (chunk) => {
+      body.push(chunk);
+      this.trace(
+        `HTTP Request: ${request.method} ${request.originalUrl}, Data event: chunk received - ${chunk.toString()}`,
+      );
+    };
+    const logRequestEnd = () => {
+      body = Buffer.concat(body).toString();
+      this.trace(
+        `HTTP Request: ${request.method} ${request.originalUrl}, End event: Request body assembled - ${body}`,
+      );
+    };
+    const logRequestError = (error) => {
+      requestErrorMessage = error.message;
+      this.trace(
+        `HTTP Request: ${request.method} ${request.originalUrl}, Error event: ${requestErrorMessage}`,
+      );
+    };
+    request.on('data', logRequestChunk);
+    request.on('end', logRequestEnd);
+    request.on('error', logRequestError);
+
+    const logClose = () => {
+      // eslint-disable-next-line no-use-before-define
+      removeHandlers();
+      this.trace(`HTTP Response: ${request.method} ${request.url}, Close event. Client aborted`);
+    };
+    const logError = (error) => {
+      // eslint-disable-next-line no-use-before-define
+      removeHandlers();
+      this.trace(`HTTP Response: ${request.method} ${request.url}, Error event: ${error.message}`);
+    };
+    const logFinish = () => {
+      // eslint-disable-next-line no-use-before-define
+      removeHandlers();
+      this.trace(
+        `HTTP Response: ${response.statusCode} ${request.method} ${request.url}, Finish event`,
+      );
+    };
+    response.on('close', logClose);
+    response.on('error', logError);
+    response.on('finish', logFinish);
+
+    const removeHandlers = () => {
+      request.off('data', logRequestChunk);
+      request.off('end', logRequestEnd);
+      request.off('error', logRequestError);
+      response.off('close', logClose);
+      response.off('error', logError);
+      response.off('finish', logFinish);
+    };
+  }
+
   get logLevelMap() {
     return {
-      TRACE: 'info',
-      DEBUG: 'info',
+      TRACE: 'debug',
+      DEBUG: 'debug',
       INFO: 'info',
       WARN: 'error',
       ERROR: 'error',
@@ -69,6 +133,15 @@ class LoggerService {
       return;
     }
     this.logger.info(message);
+  }
+
+  trace(message) {
+    if (this.logFormat === 'TEXT') {
+      // eslint-disable-next-line no-console
+      console.log(message);
+      return;
+    }
+    this.logger.debug(message);
   }
 
   error(err) {
